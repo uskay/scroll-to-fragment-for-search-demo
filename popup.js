dd43352d0a1f1653263f63ba92670025e6ed1845
uskay <uskay@google.com>
2021-04-21 13:08:58 +0900

    Removing uncessary scripts

--- popup.js
+++ popup.js
@@ -1,11 +1,6 @@
 [...document.querySelectorAll('.feature-toggle button')].forEach(btn => {
   btn.addEventListener("click", async (evt) => {
-
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      files: ['kuromoji.js']
-    });
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       files: [`${evt.target.id}.js`]
